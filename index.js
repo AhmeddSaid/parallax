40ab79a6712d75d211a642db02ab697a5988d3a5
Ahmed Said <ahmedsaidadnan@outlook.com>
2023-06-07 03:18:26 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+gsap.from("#m1", {
+  scrollTrigger: {
+    scrub: true,
+  },
+  y: 125,
+});
+gsap.from("#m2", {
+  scrollTrigger: {
+    scrub: true,
+  },
+  y: 75,
+});
+gsap.from("#t2", {
+  scrollTrigger: {
+    scrub: true,
+  },
+  x: -75,
+});
+gsap.from("#t1", {
+  scrollTrigger: {
+    scrub: true,
+  },
+  x: 75,
+});
+gsap.from("#man", {
+  scrollTrigger: {
+    scrub: true,
+  },
+  x: -250,
+});
+gsap.from("#plants", {
+  scrollTrigger: {
+    scrub: true,
+  },
+  x: -75,
+});
+gsap.from("#text", {
+  scrollTrigger: {
+    scrub: true,
+  },
+  x: 500,
+});
